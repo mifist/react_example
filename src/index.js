446fca1e203b6e756a21689f57b6dd71a211d44d
Mifist <avanturka1992@gmail.com>
2019-10-20 22:58:19 +0300

    seample html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = <h1>Helloo</h1>;
+const title = (
+    <div>
+        <h1>TODO List</h1>
+        <input placeholder="Search..." />
+        <ul>
+            <li>list 1</li>
+            <li>list 2</li>
+        </ul>
+    </div>
+);
 
 ReactDOM.render(title,
     document.getElementById('root'));
