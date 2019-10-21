7de78a2337207284bcfd231ac6199624803d38cb
Mifist <avanturka1992@gmail.com>
2019-10-21 22:07:19 +0300

    split applications into 3 independent components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = (
-    <div>
-        <h1>TODO List</h1>
-        <input placeholder="Search..." />
+const TodoList = () => {
+    return (
         <ul>
-            <li>list 1</li>
-            <li>list 2</li>
-        </ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+    </ul>
+    );
+};
+
+const AppHeader = () => {
+    return (
+        <h1>TODO List</h1>
+    );
+};
+
+const SearchPanel = () => {
+    return (
+        <input placeholder="Search..." />
+    );
+};
+
+const el = (
+    <div>
+        <AppHeader /> 
+        <SearchPanel />   
+        <TodoList />
     </div>
 );
 
-ReactDOM.render(title,
+ReactDOM.render(el,
     document.getElementById('root'));
 
 
