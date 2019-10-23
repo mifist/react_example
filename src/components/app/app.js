98d8bc779ac800b7436d4c1a85f405b60d918d22
Mifist <avanturka1992@gmail.com>
2019-10-23 22:40:26 +0300

    .setState() - deleted list item, without chenges in the previous value on the state argument

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 /* Core */
-import React from 'react';
+import React, { Component } from 'react';
 /* Component */
 import AppHeader from '../app-header';
 import AppFooter from '../app-footer';
@@ -12,30 +12,53 @@ import TodoList from '../todo-list';
 /* Style */
 import './app.scss';
 
-const App = () => {
-    const copyright = 'This is footer copyright';
-    const todoData = [
-        { label: 'Drink coffe', important: false, id: 1 },
-        { label: 'Drink water', important: false, id: 2 },
-        { label: 'Learn React', important: true, id: 3 },
-        { label: 'Learn ES6', important: true, id: 4 },
-        { label: 'Have a lanch', important: false, id: 5 },
-    ];
+export default class App extends Component {
+    state = { 
+        todoData: [
+            { label: 'Drink coffe', important: false, id: 1 },
+            { label: 'Drink water', important: false, id: 2 },
+            { label: 'Learn React', important: true, id: 3 },
+            { label: 'Learn ES6', important: true, id: 4 },
+            { label: 'Have a lanch', important: false, id: 5 },
+        ]
+    }
 
-    return (
-        <div className="main-app">
-            <AppHeader toDo="1" done="3" /> {/* name of React component */}
-            <div className="app-actions-panel">
-                <SearchPanel />   
-                <StatusFilter />
-            </div>
-            <TodoList 
-                todos={todoData} 
-                onDeleted={ (id) => console.log('del', id) }
-            />
-            <AppFooter copyright={copyright} />
-        </div> 
-    );
-};
+    /* Нельзя изменять предыдущее значение state, 
+    поскольку он очень часто изменяется асинхронно и могут возникнуть ошибки */
+    deletListItem = (id) => {
+        console.log('del', id)
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex( (el) => el.id === id )
+        /*  const before = todoData.slice(0, idx)
+            const after = todoData.slice(idx+1) */
+            const newTodoData = [
+               /*  ... before, ... after */
+               /* or */
+                ... todoData.slice(0, idx),
+                ... todoData.slice(idx+1)
+            ];
+            return {
+                todoData: newTodoData
+            }
+        });
+    }
 
-export default App;
+    render() {
+        const copyright = 'This is footer copyright';
+        const { todoData } = this.state; // arr
+        return (
+            <div className="main-app">
+                <AppHeader toDo="1" done="3" /> {/* name of React component */}
+                <div className="app-actions-panel">
+                    <SearchPanel />   
+                    <StatusFilter />
+                </div>
+                <TodoList 
+                    todos={ todoData } 
+                    onDeleted={ this.deletListItem }
+                />
+                <AppFooter copyright={copyright} />
+            </div> 
+        );
+    }
+}
